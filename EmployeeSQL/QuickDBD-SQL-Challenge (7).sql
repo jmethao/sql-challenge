4435c0e917db44ebe232d91abb875f2a398b583e
jmeethao <jmeethao@gmail.com>
2024-12-15 15:05:56 -0600

    adding updated DBD

---
+++ EmployeeSQL/QuickDBD-SQL-Challenge (7).sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/duGMlS
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" Varchar   NOT NULL,
+    "dept_name" Varchar   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no","dept_name"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" Int   NOT NULL,
+    "dept_no" Varchar   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" Varchar   NOT NULL,
+    "emp_no" Int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" Int   NOT NULL,
+    "emp_title_id" Varchar   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" Varchar   NOT NULL,
+    "last_name" Varchar   NOT NULL,
+    "sex" Varchar   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" Int   NOT NULL,
+    "salary" Int   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" Varchar   NOT NULL,
+    "title" Varchar   NOT NULL
+);
+
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_emp" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no_dept_no" FOREIGN KEY("emp_no", "dept_no")
+REFERENCES "dept_manager" ("emp_no", "dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "salaries" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
